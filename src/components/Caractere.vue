2d68b50216ff661b8878542317dc369058c788ae
MichaCham <cham7694-github@yahoo.com>
2022-03-29 15:30:42 +0200

    Debut affichage perso

--- src/components/Caractere.vue
+++ src/components/Caractere.vue
@@ -1,11 +1,17 @@
-<script>
-</script>
-
 <template>
     <h1>Caractere !!!!</h1>
 
     <RouterLink to="/">Accueil</RouterLink> <br>
     <RouterLink to="/Episode">Episode</RouterLink>
+
+    <div v-for="perso in this.personnages">
+    <img :src="perso.image">
+    <div>
+        nom :{{perso.name}}
+    </div>
+    
+    </div>
+    
 </template>
 
 <style>
@@ -13,7 +19,28 @@
 
 
 <script>
+import axios from "axios";
+import { defineComponent} from 'vue';
 
+export default defineComponent({
+    data: () => {
+        return {
+            personnages:[],
+            info:[],
+            error: "",
 
+        }
+    },mounted(){
+        axios.get("https://rickandmortyapi.com/api/character")
+        .then((data)=>{
+            this.personnages = data.data.results;
+            this.info = data.data.info;
+            console.log(this.personnages);
+        })
+         .catch((error) => {
+            this.error = error;
+        });
+    }
+})
 
 </script>
